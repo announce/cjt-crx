15b0b9832077cba79b90ad17f7e64c698bf2f9be
Kenta Yamamoto <lq33xxx@gmail.com>
2018-06-17 19:34:36 +0900

    Added popup UI to convert CSV file

--- script/manifest.json.js
+++ script/manifest.json.js
@@ -1,4 +1,4 @@
 
-const manifest = require('./manifest.json')
+const manifest = require('../src/manifest.json')
 manifest.version = process.env.npm_package_version
 console.log(JSON.stringify(manifest))
